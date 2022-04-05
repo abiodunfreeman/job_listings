04f09d26ecb01e8a3c6accb3065a842b0d9c8d6d
Odun Freeman <96088797+bornduringwar@users.noreply.github.com>
2022-04-05 11:03:12 -0400

    more css to card component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@
 import './components/css/App.css';
 import Header from './components/Header';
 import data from "./components/data"
+import Card from './components/Card';
+
 function App() {
   console.log(data)
+  const cardsJSXArray = data.map(listing => {
+    return (
+      <Card
+      key={listing.id}
+      data={listing}
+      />
+    )
+  })
   return (
     <div className="App">
       <Header/>
+      <section className="card-list-container">
+       {cardsJSXArray[0]}
+       {cardsJSXArray[3]}
+      </section>
+      
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,29 @@
 import React from 'react'
-
+import "./css/card.css"
 export default function Card( props ) {
-  return (
-    <div className="card--container">Card</div>
+    const {data} = props
+    const {company, contract, featured, id, languages, level, location, logo, position, postedAt, role, tools} = data
+    const languagesList = languages.map(element => <li>{element}</li>) // takes all languages and puts them individually in li elements
+    const newLogo = <div className='new--logo'><p>NEW!</p></div>
+    const featuredLogo = <div className='featured--logo'><p>FEATURED</p></div>
+    return (
+    <div className="card--container">
+        <div className="card--left">
+            <img src={data.logo} alt="company logo"/>
+            <div className='card--text'>
+                <div className='card--name'>
+                    <h4>{company}</h4>
+                    {data.new && newLogo}
+                    {featured && featuredLogo}
+                </div>
+                <h3>{position}</h3>
+            </div>
+        </div>
+        <div className='card--right'>
+           <ul className="card--languages">
+            {languagesList} 
+           </ul>
+        </div>
+    </div>
   )
 }
